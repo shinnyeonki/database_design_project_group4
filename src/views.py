feec9055c9067e8a0f4003049f02593410c1985c
shinnyeonki <sygys10293@gmail.com>
2024-11-17 04:23:28 +0900

    구조 개선

--- src/views.py
+++ src/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
-from app import connection
+
+connection = None
+def enroll_connection(conn):
+    global connection
+    connection = conn
 
 main = Blueprint('main', __name__)
-
 
 @main.route('/')
 def index(): #연결 test 및 home.html 렌더링
