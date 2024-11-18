e147f11c8b661e2968bddb4972cc5eaccdcf3ce5
shinnk <sygys10293@gmail.com>
2024-11-18 23:22:45 +0900

    oracle driver 환경구성

--- src/app.py
+++ src/app.py
@@ -1,13 +1,18 @@
 import os
 from flask import Flask
+import platform
 import cx_Oracle
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    # oracle 드라이버 설정
-    cx_Oracle.init_oracle_client(lib_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'instantclient_23_6'))
-    # 데이터베이스 연결 설정
+    if os.name == "nt":
+        cx_Oracle.init_oracle_client(lib_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'instantclient_23_6'))
+    elif os.name == "posix":
+        if platform.system() == "Linux":
+            cx_Oracle.init_oracle_client(lib_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'instantclient_23_6'))
+        elif platform.system() == "Darwin":  # macOS는 Darwin으로 식별됩니다.
+            cx_Oracle.init_oracle_client(lib_dir=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'instantclient_23_3'))
     dsn = cx_Oracle.makedsn("shinnk.iptime.org", 11522, sid="XE")
     connection = cx_Oracle.connect(user="db_project_group4", password="1111", dsn=dsn)
     from views import main, enroll_connection
